00bd93d9059c156ef841a551313079dcd12a1748
Unknown <mathewhorner123@gmail.com>
2018-04-18 22:25:21 -0500

    Working on schedule form.

--- src/scheduleComponent.js
+++ src/scheduleComponent.js
@@ -3,7 +3,42 @@ import React, { Component } from 'react';
 class Schedule extends Component {
 	render() {
 		return (
-			<div></div>
+			<div className = "container-fluid">
+				<div class = "row">
+					<div class = "col-6">
+						<p>Google Calendar lies here.</p>
+					</div>
+					<div class = "col-6">
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text" id="basic-addon1">Recipe Name</span>
+							</div>
+							<input type="text" class="form-control" placeholder="" aria-label="RecipeName" aria-describedby="basic-addon1"/>
+						</div>
+						
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text" id="basic-addon2">Recipe URL</span>
+							</div>
+							<input type="text" class="form-control" placeholder="" aria-label="RecipeURL" aria-describedby="basic-addon2"/>
+						</div>
+						
+						<div class = "col-6">
+							<div class = "input-group mb-3">
+								<select class="custom-select" id="month">
+								</select>
+								<select class="custom-select" id="day">
+								</select>
+								<select class="custom-select" id="year">
+								</select>
+							</div>
+						</div>
+						
+						<div class = "col-6">
+						</div>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
