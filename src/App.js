9fd9eada6d833806f985fe9479435dbca0e65f02
Unknown <mathewhorner123@gmail.com>
2018-04-18 19:41:00 -0500

    getting loginw orking

--- src/App.js
+++ src/App.js
@@ -16,12 +16,28 @@ import RecipeView from './recipeView';
 import { GoogleLogout, GoogleLogin } from 'react-google-login';
 	
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.responseGoogleSuccess = this.responseGoogleSuccess.bind(this);
+		
+		this.state = {
+			loggedIn: false,
+			email: "",
+			picUrl: ""
+		};
+	}
+	
 	responseGoogleSuccess(response) {
 		console.log("Success", response);
+		this.setState({loggedIn: true});
+		this.setState({email: response.profileObj.email});
+		this.setState({picUrl: response.profileObj.imageUrl});
 	}
+	
 	responseGoogleFail(response) {
 		console.log("Fail", response);
 	}
+	
   render() {
     return (
 		<div className="App">
@@ -40,12 +56,23 @@ class App extends Component {
 							<li class = "nav-item"><a href = '/schedule' id = "navStyle" class = "nav-link">Schedule</a></li>
 						</ul>
 						
+						{!this.state.loggedIn &&
 						<GoogleLogin
-							clientId = ""
+							clientId = "122146894892-ouqdfmr7105f55aiim0vg1glrcf0atnk.apps.googleusercontent.com"
 							buttonText = "Login"
 							onSuccess = {this.responseGoogleSuccess}
 							onFailure = {this.responseGoogleFail}
 						/>
+						}
+						
+						{this.state.loggedIn &&
+						<div>
+							<p>Profile Details</p>
+							<img src = {this.state.picUrl} id = "pic" class = "img-circle" width = "100" height = "100" />
+							<p>Email Address</p>
+							<p id = "email" class = "alert alert-danger">{this.state.email}</p>
+						</div>
+						}
 					</div>
 				</div>
 			</nav>
